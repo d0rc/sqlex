8544e5ce837c66362863d0e231f9d9dd2f30373c
d0rc <7993863@gmail.com>
2013-07-22 02:23:29 +0000

    query works

--- lib/sqlex.ex
+++ lib/sqlex.ex
@@ -7,6 +7,18 @@ defmodule SQL do
 		name_list = lc :field[name: name] inlist fields, do: to_atom name
 		lc row inlist rows, do: Enum.zip(name_list, row)
 	end
+
+	defp prep_argument(arg) when is_list(arg), do: [[?(| :erlang.binary_to_list Enum.join arg, "," ]|[?)]]
+	defp prep_argument(arg) when is_binary(arg), do: [[?'|:erlang.binary_to_list(arg)]|[?']] 
+	defp prep_argument(arg) when is_integer(arg), do: :erlang.integer_to_list arg
+
+	defp in_query([], _), do: []
+	defp in_query(sql, args) when is_binary(sql), do: in_query(:erlang.binary_to_list(sql), args)
+	defp in_query([??|sql], [arg|arg_tail]), do: [prep_argument(arg)|in_query(sql, arg_tail)]
+	defp in_query([s|sql], args), do: [s|in_query(sql, args)]
+
+	def query(sql, args), do: :erlang.list_to_binary List.flatten in_query sql, args
+
 	def init_pool do
 		:ok = :emysql.add_pool :mp, 5, 'root', '', 'lotod3', 3306, 'bm', :utf8
 	end
--- test/sqlex_test.exs
+++ test/sqlex_test.exs
@@ -6,8 +6,7 @@ defmodule SqlexTest do
   test "we can start apps" do
   	assert( Enum.all? (lc a inlist [:crypto, :emysql], do: :application.start a), fn r ->
   		{:error, {:already_started, _}} = r
-  	end)
-	 	 
+  	end)	 
   end
 
   test "we can run query" do
@@ -17,5 +16,25 @@ defmodule SqlexTest do
 
   test "the truth" do
     assert(true)
+  end
+
+  test "prepare works" do
+  	assert "select hello 'world'? " == SQL.query "select hello ?? ", ["world"]
+  end
+
+  test "prepare works for several args" do
+  	assert "select hello, 'world' and 'joe'" == SQL.query "select hello, ? and ?", ["world", "joe"]
+  end
+
+  test "prepare works for numbers as well" do
+  	assert "select hello 1980" == SQL.query "select hello ?", [1980]
+  end
+
+  test "prepare works for numbers and binaries" do
+  	assert "select 'hello' 1980" == SQL.query "select ? ?", ["hello", 1980]
+  end
+
+  test "prepare works for lists" do
+  	assert "select * from posts where id in (1,2,300)" == SQL.query "select * from posts where id in ?", [[1,2,300]]
   end
 end
